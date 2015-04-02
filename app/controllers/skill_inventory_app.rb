20a05ec7b99bb5191eafb57ae6ecd4c04004589b
Jack Yeh <jack.yeh@gmail.com>
2015-04-01 20:19:54 -0600

    skill inventory works but has no extensions

---
+++ app/controllers/skill_inventory_app.rb
@@ -0,0 +1,28 @@
+require 'models/skill_inventory'
+
+class SkillInventoryApp < Sinatra::Base
+  set :root, File.join(File.dirname(__FILE__), '..')
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/skills' do
+    @skills= SkillInventory.all
+    erb :index
+  end
+
+  get '/skills/new' do
+    erb :new
+  end
+
+  post '/skills' do
+    SkillInventory.create(params[:skill])
+    redirect '/skills'
+  end
+
+  get '/skills/:id' do |id|
+    @skill= SkillInventory.find(id.to_i)
+    erb :show
+  end
+end
---
+++ app/models/skill_inventory.rb
@@ -0,0 +1,35 @@
+require 'yaml/store'
+require_relative 'skill'
+
+class SkillInventory
+  def self.database
+    @database ||= YAML::Store.new("db/skill_inventory")
+  end
+
+  def self.create(skill)
+    database.transaction do
+      database['skills'] ||= []
+      database['total'] ||= 0
+      database['total'] += 1
+      database['skills'] << { "id" => database['total'], "title" => skill[:title], "description" => skill[:description] }
+    end
+  end
+
+  def self.raw_skills
+    database.transaction do
+      database['skills'] || []
+    end
+  end
+
+  def self.all
+    raw_skills.map { |data| Skill.new(data) }
+  end
+
+  def self.raw_skill(id)
+    raw_skills.find { |skill| skill["id"] == id }
+  end
+
+  def self.find(id)
+    Skill.new(raw_skill(id))
+  end
+end
